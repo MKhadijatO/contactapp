634c10b2b2ffc907e64059a51fb9bf508218c48e
Khadijat <muhammadkhadijato@gmail.com>
2024-02-16 23:37:03 +0100

    update

--- contactopedia/src/Components/ContactsPages/ContactIndex.jsx
+++ contactopedia/src/Components/ContactsPages/ContactIndex.jsx
@@ -46,8 +46,19 @@ class ContactIndex extends React.Component {
     };
   }
 
-  handleAddContact = () => {
+  handleAddContact = (newContact) => {
+    const newFinalContact = {
+      ...newContact,
+      id: this.state.contactList[this.state.contactList.length - 1].id + 1,
+      isFavorite: false,
+    };
+    this.setState((prevState) => {
+      return{
+        contactList: prevState.contactList.concat([newFinalContact]),
+      }
+    });
     alert("Contact has been added");
+
   };
 
   render() {
@@ -62,27 +73,29 @@ class ContactIndex extends React.Component {
             <div className="col-4">
               <RemoveAllContacts />
             </div>
-            
+
             <div className="row py-3">
-            <div className="col-8 offset-2 row">
-              <AddContact  handleAddContact= {this.handleAddContact}/>
-              </div>
-            </div>
-            <div className="row py-3">
-            <div className="col-8 offset-2 row">
-              <FavoriteContacts
-                contacts={this.state.contactList.filter(
-                  (u) => u.isFavorite === true
-                )}
-              />
+              <div className="col-8 offset-2 row">
+                <AddContact handleAddContact={this.handleAddContact} />
               </div>
             </div>
             <div className="row py-3">
               <div className="col-8 offset-2 row">
-              <GeneralContacts contacts={this.state.contactList.filter(
-                  (u) => u.isFavorite === false
-                )}/>
-                </div>
+                <FavoriteContacts
+                  contacts={this.state.contactList.filter(
+                    (u) => u.isFavorite === true
+                  )}
+                />
+              </div>
+            </div>
+            <div className="row py-3">
+              <div className="col-8 offset-2 row">
+                <GeneralContacts
+                  contacts={this.state.contactList.filter(
+                    (u) => u.isFavorite === false
+                  )}
+                />
+              </div>
             </div>
           </div>
         </div>
